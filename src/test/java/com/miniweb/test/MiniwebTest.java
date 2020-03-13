e6512f65f8bd0de1b0d3aa9a400069c288861386
PSEH-5 <indiahiring@global.publicisgroupe.net>
2020-03-13 12:03:42 +0530

    2nd commit

--- src/test/java/com/miniweb/test/MiniwebTest.java
+++ src/test/java/com/miniweb/test/MiniwebTest.java
@@ -1,19 +1,16 @@
 package com.miniweb.test;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import com.miniweb.api.RestAPI;
 import com.miniweb.model.Standings;
 
 public class MiniwebTest {
 
-	RestAPI restAPI = new RestAPI();
+	RestAPI restAPI = null;
 
 	@Before
 	public void setupInitial() throws Exception {
@@ -24,6 +21,7 @@ public class MiniwebTest {
 	public void getgetByCountryTest() {
 		List<Standings> standingsList = new ArrayList<>();
 		try {
+			restAPI = new RestAPI();
 			standingsList = restAPI.getByCountry("England");
 			System.out.println(standingsList.toString());
 			assertNotNull(standingsList);
@@ -36,6 +34,7 @@ public class MiniwebTest {
 	public void getgetStandingsTest() {
 		String result = "";
 		try {
+			restAPI = new RestAPI();
 			result = restAPI.getStandings();
 			System.out.println(result);
 			assertNotNull(result);
